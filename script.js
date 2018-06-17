9265ae447af272849294d3f9b8d3481fd18063f6
Fahad <ifahaduddin@gmail.com>
2018-06-17 05:13:40 +0500

    Removed unwanted code

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 
 $(document).ready(function(){
 	var count=localStorage.length;// get count
-	//console.log($('.data').val());	// whats in the todo 0
-	//console.log(localStorage.length); // how much data is in the storage
-
-	var data = new Array();
-	
+	var data = new Array();	
 	var i=0;
-
 		while (i<localStorage.length) {
 			if(!localStorage.key(i))
 			{
@@ -15,68 +10,43 @@ $(document).ready(function(){
 				continue;
 			}
 			data[i]=localStorage.getItem(localStorage.key(i));
-			//console.log(data[i]);
 			i++;
 		}
-
-	//var count=localStorage.getItem("todo").length;
 	$(".data").keypress(function(e){
 		if(e.which == 13) {
-			//count++;		
 			console.log("pressed");
 		data.push($('.data').val());
 		localStorage.setItem("todo"+(localStorage.length+1),$('.data').val());
 		count++;
 		var i=0;
-		//$(".todorow").addClass("hide");
-		$(".todorow").remove();
-		//$(".data").html("");
-		
+		$(".todorow").remove();		
 		while (i<localStorage.length) {
 			var value = "<div class='todorow'><div id="+localStorage.key(i)+" class='todoitem'>"+data[i]+"</div><div class='todoremove'>X</div></div>";
 			$(".container").append(value);
-			//console.log(data[i]);
 			i++;
 		}
 		$("input").val("");
-		}
-		//$('input').attr('value')= '';
-		
+		}		
     });
     $(".submit").click(function(){
-		//count++;		
 		data.push($('.data').val());
 		localStorage.setItem("todo"+(localStorage.length+1),$('.data').val());
 		count++;
 		var i=0;
-		//$(".todorow").addClass("hide");
-		$(".todorow").remove();
-		//$(".data").html("");
-		
+		$(".todorow").remove();		
 		
 		while (i<localStorage.length) {
 			var value = "<div class='todorow'><div id="+localStorage.key(i)+" class='todoitem'>"+data[i]+"</div><div class='todoremove'>X</div></div>";
 			$(".container").append(value);
-			//console.log(data[i]);
 			i++;
 		}
-		//$('input').attr('value')= '';
 		$("input").val("");
 		
     });
-	/*
-    $(".container").on("click", ".todoitem", function(){
-        jQuery(this).toggleClass("strike");
-    });
-	*/
     $(".container").on("click", ".todoremove", function(){
-        //jQuery(this).closest('.todorow').addClass("hide");
-		//console.log(jQuery(this).siblings().attr('id'));
 		localStorage.removeItem(jQuery(this).siblings().attr('id'));
 		console.log(jQuery(this).siblings().attr('id'));
-		//$(".todorow").addClass("hide");
 		$(".todorow").remove();
-	
 		var data2 = new Array();
 		var j=0;
 		while (j<localStorage.length) {
@@ -84,10 +54,8 @@ $(document).ready(function(){
 			{
 				j++;
 				continue;
-			}
-				
+			}	
 			data2[j]=localStorage.getItem(localStorage.key(j));
-			//console.log(data2[j]);
 			j++;
 		}
 		data = data2.slice(0);
@@ -101,12 +69,10 @@ $(document).ready(function(){
 			}
 			var value = "<div class='todorow'><div id="+localStorage.key(i)+" class='todoitem'>"+localStorage.getItem(localStorage.key(i))+"</div><div class='todoremove'>X</div></div>";
 			$(".container").append(value);
-			//console.log(data[i]);
 			i++;
 		}
 		
     });
-	
 	var i=0;
 		while (i<localStorage.length) {
 			if(!localStorage.key(i))
@@ -117,8 +83,6 @@ $(document).ready(function(){
 			}
 			var value = "<div class='todorow'><div class='todoitem'>"+data[i]+"</div><div class='todoremove'>X</div></div>";
 			$(".container").append(value);
-			//console.log(data[i]);
 			i++;
 		}
-	
 });
